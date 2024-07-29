29a4a0a3eaa940d427e85976f6beb43f2b716e4c
Maria Arthur Gyamfi <abenajemfy@gmail.com>
2024-07-29 23:27:15 +0000

    Created files for a web application for taking orders

---
+++ WebApp/product.js
@@ -0,0 +1,59 @@
+const products = [
+  {
+    id: 1,
+    name: "Microwave",
+    price: 100,
+    description: "products you will love",
+    image: "images/microwave-removebg-preview.png",
+  },
+  {
+    id: 2,
+    name: "Blender",
+    price: 100,
+    description: "products you will love",
+    image: "images/blender-removebg-preview.png",
+  },
+  {
+    id: 3,
+    name: "Oven",
+    price: 100,
+    description: "products you will love",
+    image: "images/oven-removebg-preview.png",
+  },
+  {
+    id: 4,
+    name: "Cooker",
+    price: 100,
+    description: "products you will love",
+    image: "images/rice_cooker-removebg-preview.png",
+  },
+  {
+    id: 5,
+    name: "Juicer",
+    price: 100,
+    description: "products you will love",
+    image: "images/juicer-removebg-preview.png",
+  },
+  {
+    id: 6,
+    name: "Gas Burner",
+    price: 100,
+    description: "products you will love",
+    image: "images/gas-3-burner-removebg-preview.png",
+  },
+  {
+    id: 7,
+    name: "Air Fryer",
+    price: 100,
+    description: "products you will love",
+    image: "images/air_fryer-removebg-preview (1).png",
+  },
+  {
+    id: 8,
+    name: "Toaster",
+    price: 100,
+    description: "products you will love",
+    image: "images/toaster-removebg-preview.png",
+  },
+];
+export default products;
